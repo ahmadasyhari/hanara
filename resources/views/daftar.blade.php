393b41b9468d41c41407b8c1df3fabfa13019ef4
ahmadasyhari <gibran.nur.ahmad@gmail.com>
2023-10-05 07:53:59 +0700

    updat form pendaftaran

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -3,8 +3,21 @@
 
 @section('content')
             <h1>Silahkan Masukkan Nama Dan Data Diri Yang Lengkap Dan Benar</h1>
-            Nama Lengkpan : <input type="text"> <br>
-            Tanggal       : <input type="date" name="tanggal" id="pilih"><br>
-            Kelas         : <input type="checkbox" name="pilihkelas" id="pkl"> <br>
-            <button>Simpan</button>
+                    <form method="POST" action="">
+                        @csrf
+                        <div class="form-group">
+                            <label for="nama">Nama:</label>
+                            <input type="text" name="nama" id="nama" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="usia">Usia:</label>
+                            <input type="text" name="usia" id="usia" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="hobi">Hobi:</label>
+                            <input type="text" name="hobi" id="hobi" class="form-control" required>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </form>
 @endsection
